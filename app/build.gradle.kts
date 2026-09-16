9f0686f6d0b008170af3fa133552d5a5877fda32
ffontinele <seu_email@exemplo.com>
2026-09-16 16:51:43 -0400

    feat: v0.33.0 gold YouTube Pro icon + alternative icons stored

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.youtube.pro.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.32.1"
+        versionCode = 2
+        versionName = "0.33.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "app_name", "YouTube Pro")
     }
